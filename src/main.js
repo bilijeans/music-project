bce77e5220af1352f26ae2a23e258e2549dc56f7
deeray <1223921695@qq.com>
2022-10-11 11:46:10 +0800

    init project

--- src/main.js
+++ src/main.js
@@ -1,12 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-import store from './store'
+import VueAxiosPlugin from 'vue-axios-plugin'
 
-Vue.config.productionTip = false
 
-new Vue({
-  router,
-  store,
-  render: h => h(App)
-}).$mount('#app')
+Vue.use(VueAxiosPlugin, {
+  // 请求拦截处理
+  reqHandleFunc: config => config,
+  reqErrorFunc: error => Promise.reject(error),
+  // 响应拦截处理
+  resHandleFunc: response => response,
+  resErrorFunc: error => Promise.reject(error)
+})
