81a2122298434724358058e0c6f86eeab9bacf34
deeray <1223921695@qq.com>
2022-10-11 12:04:00 +0800

    project init

---
+++ src/main.js
@@ -0,0 +1,22 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+import VueAxiosPlugin from 'vue-axios-plugin'
+import wotUI from './wotui'
+Vue.use(wotUI)
+Vue.use(VueAxiosPlugin, {
+  // 请求拦截处理
+  reqHandleFunc: config => config,
+  reqErrorFunc: error => Promise.reject(error),
+  // 响应拦截处理
+  resHandleFunc: response => response,
+  resErrorFunc: error => Promise.reject(error)
+})
+Vue.config.productionTip = false
+
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount('#app')
