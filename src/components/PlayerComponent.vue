a3c33391983f26c06ee4f0ad36ba2ea30bb4d36a
一哥 <11629220+bro-one@user.noreply.gitee.com>
2022-10-17 10:34:15 +0800

    fix clash by bilijeans

--- src/components/PlayerComponent.vue
+++ src/components/PlayerComponent.vue
@@ -226,12 +226,7 @@ export default {
     height: 50px;
     border-radius: 999px;
     overflow: hidden;
-<<<<<<< HEAD
     background-image: url("@/assets/Album.png");
-=======
-    background-image: url('@/assets/Album.png');
-
->>>>>>> f2b1612cb3ab4d419d9f5fb1968ec7e95ce50dbf
     background-size: cover;
     background-repeat: no-repeat;
     img {
