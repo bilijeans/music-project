08a6f760a951a8d8ab721eda3ed56e0ff38e1a5a
一哥 <11629220+bro-one@user.noreply.gitee.com>
2022-10-17 09:13:44 +0800

    write playbar and playlist by bilijeans

--- src/components/PlayListComponent.vue
+++ src/components/PlayListComponent.vue
@@ -1,14 +1,135 @@
 <template>
-  <div></div>
+  <transition name="list">
+    <div class="wait-song-play-list">
+      <div class="header">
+        <span @click="packUpList">收起</span>
+      </div>
+      <ul class="items">
+        <li
+          class="item"
+          ref="songItem"
+          v-for="i in listData"
+          :key="i.songId"
+          @click="playSong(i.songId)"
+        >
+          <span class="name">{{ i.name }}</span>
+          <span class="singer">{{ " - " + dealWithSingerName(i.singer) }}</span>
+        </li>
+      </ul>
+    </div>
+  </transition>
 </template>
 <script>
-import { mapState } from "vuex";
+import { mapState, mapActions } from "vuex";
 export default {
   data() {
-    return {};
+    return {
+      show: true,
+    };
   },
-  computed:{
-    ...mapState([])
-  }
+  computed: {
+    ...mapState({
+      playId: (state) => state.playbar.playId,
+    }),
+    ...mapState(["playbar"]),
+    ...mapState({
+      listData: (state) => state.playList.listData,
+    }),
+  },
+  watch: {
+    playId() {
+      console.log(this.listData);
+      this.listData.forEach((e, i) => {
+        if (e.songId == this.playId) {
+          console.log(this.$refs.songItem[i]);
+          this.$nextTick(() => {
+            this.$refs.songItem[i].className = "item active";
+          });
+        } else {
+          this.$nextTick(() => {
+            this.$refs.songItem[i].className = "item";
+          });
+        }
+      });
+    },
+  },
+  created() {
+    console.log(this.listData);
+  },
+  mounted() {
+    console.log(this.listData);
+  },
+  methods: {
+    dealWithSingerName(arr) {
+      let str = "";
+      arr.forEach((e) => {
+        str = str + e.name + "/";
+      });
+      str = str.slice(0, -1);
+      return str;
+    },
+    packUpList() {
+      // this.show = !this.show
+      this.$emit("packUp");
+    },
+    playSong(id) {
+      this.listData.forEach((e, i) => {
+        if (e.songId == id) {
+          this.$nextTick(() => {
+            this.$refs.songItem[i].className = "item active";
+          });
+        }
+      });
+      console.log(this.$refs.songItem);
+      this.play(id);
+    },
+    ...mapActions({
+      play: "getPlayURL",
+    }),
+  },
 };
 </script>
+<style lang="scss">
+.wait-song-play-list {
+  position: fixed;
+  bottom: 0px;
+  left: 0px;
+  width: 100vw;
+  height: 60vh;
+  background-color: rgba(255, 255, 255);
+  .header {
+    width: 100%;
+    height: 40px;
+    line-height: 40px;
+    background-color: red;
+    span {
+      position: absolute;
+      right: 20px;
+    }
+  }
+  .items {
+    .item {
+      padding: 10px 20px;
+      font-size: 14px;
+      .singer {
+        font-size: 12px;
+        color: #999;
+      }
+    }
+  }
+}
+.list-enter {
+  height: 0px;
+}
+
+.list-leave {
+  height: 60vh;
+}
+.list-leave-to {
+  height: 0;
+}
+.list-enter-active,
+.list-leave-active {
+  transition: all 0.2s ease-in;
+}
+</style>
--- src/store/playList.js
+++ src/store/playList.js
@@ -1,10 +1,35 @@
 export default {
-    state:{
-        listData:[]
+    state: {
+        listData: []
+        /*
+        {
+            name:最伟大的作品，
+            singger：周杰伦
+            songID：1139846641
+        }
+         */
     },
-    mutations:{
-        addToList(state,data){
-            state.listData = [...state.listData,...[data]]
+    mutations: {
+        addToList(state, data) {
+            // state.listData = [...state.listData,...[data]]
+            let newArr = []
+            let newSong = {
+                name: data.songName,
+                singer: data.singerList,
+                songId: data.id
+            }
+            let hasSong = false
+            for (let i = 0; i < state.listData.length; i++) {
+                if (state.listData[i].songId == data.id) {
+                    hasSong = true
+                    break
+                } else {
+                    newArr.push(state.listData[i])
+                }
+            }
+            if (!hasSong) {
+                state.listData = [...[newSong], ...newArr]
+            }
         }
     }
 }
